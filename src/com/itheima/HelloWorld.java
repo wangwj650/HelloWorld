d68aec1ea82180d5734beb081d80620b74df1723
wangwj650 <wangwj650@163.com>
2019-12-30 15:55:36 +0800

    提交到dev分支

--- src/com/itheima/HelloWorld.java
+++ src/com/itheima/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Git第1次提交");
         System.out.println("Git第2次提交");
+
+        System.out.println("dev分支第1次提交");
     }
 }
