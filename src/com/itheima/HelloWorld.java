daee620d1732a815ff80080f5b3596bc5e552de2
wangwj650 <wangwj650@163.com>
2019-12-30 15:34:29 +0800

    HelloWorld第2次提交

--- src/com/itheima/HelloWorld.java
+++ src/com/itheima/HelloWorld.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Git第1次提交");
+        System.out.println("Git第2次提交");
     }
 }
